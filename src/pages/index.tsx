52901d5587b1478d5b7ed04f05892f2665c1d6f8
Robson Fischer <fischerrobson@gmail.com>
2021-04-22 22:07:50 -0300

    fim da terceira aula

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { api } from "../services/api";
 import { format, parseISO } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import convertDurationToTimeString from "../utils/convertDurationToTimeString";
+import Link from "next/link";
 
 import styles from "./home.module.scss";
 
@@ -41,7 +42,9 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
                   alt={episode.title}
                 />
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                  <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -55,7 +58,51 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
           })}
         </ul>
       </section>
-      <section className={styles.allEpisodes}></section>
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+        <table cellSpacing="0">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podecast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map((episode) => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button>
+                      <img src="/play-green.svg" alt="tocar episódio" />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 }
